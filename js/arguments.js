be26c4039ea19cfe8f1a3bb38bd225bc1cc554c0
zannabianca1997 <zannabianca199712@gmail.com>
2016-06-04 17:56:49 +0200

    Starting Code

---
+++ js/arguments.js
@@ -0,0 +1,44 @@
+//current opened argument
+openedArgument = null;
+
+//open an argument
+function openArgument(argument) {
+    closeGenerals(); //arguments display instantly
+    closeArgument();
+    visualize(argument);
+    openedArgument = argument;
+}
+    
+//close general elements, open argument ones
+function closeGenerals() {
+    unvisualize("#general");
+    visualize("#argument");
+}
+
+//close general elements, open argument ones
+function openGenerals() {
+    visualize("#general");
+    unvisualize("#argument");
+}
+
+//close current opened argument
+function closeArgument() {
+    if (openedArgument) {
+        unvisualize(openedArgument);
+    }
+}
+
+//visualize class
+function visualize(className) {
+    var ArgElements = document.querySelectorAll(className);
+    for (var i = 0; i < ArgElements.length; i++) {
+        ArgElements[i].style.display = 'inherit';
+    }
+}
+//unvisualize class
+function unvisualize(className) {
+    var ArgElements = document.querySelectorAll(className);
+    for (var i = 0; i < ArgElements.length; i++) {
+        ArgElements[i].style.display = 'none';
+    }
+}
